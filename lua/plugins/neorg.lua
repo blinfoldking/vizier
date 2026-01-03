6ebf02c0812385712e122ea401e69b6b9a1ec8e7
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2026-01-03 13:45:00 +0700

    feat: configure neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,6 +1,44 @@
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.norg" },
+	command = "set conceallevel=3",
+})
+
 return {
 	"nvim-neorg/neorg",
 	lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 	version = "*", -- Pin Neorg to the latest stable release
 	config = true,
+	opts = {
+		load = {
+			["core.defaults"] = {},
+			["core.concealer"] = {},
+			["core.journal"] = {
+				config = {
+					strategy = "flat",
+				},
+			},
+			["core.export"] = {},
+			["core.export.markdown"] = {},
+			["core.summary"] = {},
+			["core.completion"] = {
+				config = {
+					engine = "nvim-cmp",
+				},
+			},
+			["core.integrations.nvim-cmp"] = {},
+			["core.integrations.telescope"] = {},
+			["core.esupports.metagen"] = {
+				config = {
+					-- Ensure auto_update_date is true (it usually is by default)
+					update_date = true,
+					timezone = "utc",
+					type = "auto",
+				},
+			},
+			["core.esupports.hop"] = {},
+			["core.integrations.treesitter"] = {},
+			["core.esupports.indent"] = {},
+		},
+	},
+	dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "jsonc" },
+				ensure_installed = { "lua", "jsonc", "norg", "markdown", "html" },
 			})
 
 			require("nvim-treesitter.configs").setup({
