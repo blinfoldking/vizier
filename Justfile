5342b7373ccf8072c467c668c5be4967d9ac42a3
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2026-02-21 01:28:34 +0700

    feat: adjust default log behaviour

--- Justfile
+++ Justfile
@@ -1,8 +1,8 @@
 dev:
-  @RUST_LOG=debug,serenity=off,tracing=off,hyper=off,h2=off,rustls=off,reqwest=off,tungstenite=off,rig=off,sqlx=off cargo run -- run --config .vizier.toml
+  @cargo run -- run --config .vizier.toml
 
 run:
-  @RUST_LOG=debug,serenity=off,tracing=off,hyper=off,h2=off,rustls=off,reqwest=off,tungstenite=off,rig=off,sqlx=off cargo run --release -- run --config .vizier.toml
+  @cargo run --release -- run --config .vizier.toml
 
 docker:
   @docker-compose down && docker-compose up -d
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
+use std::env;
+
 use anyhow::Result;
 
 mod agent;
@@ -16,6 +18,23 @@ mod vizier;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 8)]
 async fn main() -> Result<()> {
-    pretty_env_logger::init();
+    if env::var("RUST_LOG").is_err() {
+        pretty_env_logger::formatted_builder()
+            .filter_level(log::LevelFilter::Debug)
+            .filter_module("rig", log::LevelFilter::Error)
+            .filter_module("serenity", log::LevelFilter::Error)
+            .filter_module("sqlx", log::LevelFilter::Error)
+            .filter_module("reqwest", log::LevelFilter::Error)
+            .filter_module("hyper", log::LevelFilter::Error)
+            .filter_module("tungstenite", log::LevelFilter::Error)
+            .filter_module("sqlx", log::LevelFilter::Error)
+            .filter_module("h2", log::LevelFilter::Error)
+            .filter_module("tracing", log::LevelFilter::Off)
+            .filter_module("rustls", log::LevelFilter::Off)
+            .init();
+    } else {
+        pretty_env_logger::init();
+    }
+
     vizier::cli().await
 }
