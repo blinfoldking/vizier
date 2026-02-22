ad2fb3de44f9391920d953ea7e4f1baf42ebbd9e
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2026-02-22 12:35:26 +0700

    fix: default config path

--- src/constant.rs
+++ src/constant.rs
@@ -1,4 +1,4 @@
-pub const DEFAULT_CONFIG_PATH: &str = "config/.vizier/config.toml"; // relative to $HOME
+pub const DEFAULT_CONFIG_PATH: &str = "config.toml"; // relative to $HOME
 pub const DEFAULT_CONFIG_TOML: &str = include_str!("../templates/.vizier.template.toml");
 
 pub const BOOT_MD: &str = include_str!("../templates/BOOT.md");
