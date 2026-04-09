35dd32b20fe7f4b88d76c221a959a2d238c2f57a
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2026-04-09 14:25:57 +0700

    chore: rename template file

--- src/constant.rs
+++ src/constant.rs
@@ -3,4 +3,4 @@ pub const DEFAULT_CONFIG_PATH: &str = ".vizier.yaml"; // relative to $HOME
 pub const AGENT_MD: &str = include_str!("../templates/AGENT.md");
 pub const IDENT_MD: &str = include_str!("../templates/IDENTITY.md");
 
-pub const AGENT_TEMPLATE: &str = include_str!("../templates/.agent.md");
+pub const AGENT_TEMPLATE: &str = include_str!("../templates/agent.template.md");
