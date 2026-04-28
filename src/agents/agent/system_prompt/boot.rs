0a8be23469565caf2f38f534be2fc841593b7574
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2026-04-28 20:26:49 +0700

    feat: add additional context around time

--- src/agents/agent/system_prompt/boot.rs
+++ src/agents/agent/system_prompt/boot.rs
@@ -1,4 +1,4 @@
-use chrono::Utc;
+use chrono::{Datelike, Local, Utc};
 
 pub fn boot_md() -> String {
     let heartbeat_instruction = r#"## Heartbeat — Autonomous Background Tasks
@@ -11,25 +11,55 @@ Write tasks to `HEARTBEAT.md` to execute them on a schedule. Clear the file to s
 - Use **scheduled tasks** for specific times; use **heartbeat** for continuous polling
 "#;
 
-    format!(
+    let utc_now = Utc::now();
+    let local_now = Local::now();
+
+    utc_now.weekday().number_from_sunday();
+
+    let get_day = |day: u32| match day - 1 {
+        0 => "Sunday".to_owned(),
+        1 => "Monday".to_owned(),
+        2 => "Tuesday".to_owned(),
+        3 => "Wednesday".to_owned(),
+        4 => "Thursday".to_owned(),
+        5 => "Friday".to_owned(),
+        _ => "Saturday".to_owned(),
+    };
+
+    let utc_day = get_day(utc_now.weekday().number_from_sunday());
+    let local_day = get_day(utc_now.weekday().number_from_sunday());
+
+    let res = format!(
         r#"# BOOT.md - Operating Doctrine
 
-**Date**: {}
+## Time
+
+**System Datetime (UTC)**: {}, {}
+**Actual Datetime (Local Timezone)**: {}, {}
+
+user most likely will use the actual the actual datetime, but you always use the system datetime to interact with available tools and system.
+
+## Operation Guideline
 
 1. **Check Docs** - AGENT.md (conduct), IDENTITY.md (who you are)
-2. **No Redundancy** - avoid duplicating info across documents, memory, skills
-3. **Check Metadata** - know your context (discord, websocket, etc.)
-4. **Use Tools** - leverage available tools to complete tasks
-5. **Create Skills** - write reusable instruction documents
-6. **Programmatic Sandbox** - use programmatic sandbox, when available, to construct complex multiple tool calling logic
+2. **Auto-improve** - auto improve yourselves by updating AGENT.md (conduct), IDENTITY.md 
+3. **No Redundancy** - avoid duplicating info across documents, memory, skills
+4. **Check Metadata** - know your context (discord, websocket, etc.)
+5. **Use Tools** - leverage available tools to complete tasks
+6. **Create Skills** - write reusable instruction documents
+7. **Programmatic Sandbox** - use programmatic sandbox, when available, to construct complex multiple tool calling logic
 
 ## Context Priority
-1. **Skill** → additional capabilities/instructions
-2. **Document** → user-provided documents
-3. **Memory** → long-term facts/context
+1. AGENT.md and IDENTITY.md
+2. **Skill** → additional capabilities/instructions
+3. **Shared Document** → document to collaborate with other agents across session
+4. **Memory** → long-term facts/context
 
 {}"#,
-        Utc::now().to_rfc3339(),
-        heartbeat_instruction,
-    )
+        utc_day, utc_now, local_day, local_now, heartbeat_instruction,
+    );
+
+    println!("{}", res);
+
+    res
 }
